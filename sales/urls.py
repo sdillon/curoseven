c0b9d9d5384cf852700b5a9fe5ca79865d08e518
tdiethe <tdiethe@gmail.com>
2013-09-11 07:35:22 +0000

    Revert "Django version of the static html site. A couple of small bugs, but"
    This reverts commit cd3f306f867a2ae5c5c996d8c489ace7e7afbdec.

--- sales/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-from c7.sales import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
-)
--- sales/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-
-
-def index(request):
-    template = loader.get_template('index.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
